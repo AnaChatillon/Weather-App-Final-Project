7e97b1028ffa9ad20a875a951a92125297e911c3
Ana Chatillon <a_chantil@hotmail.com>
2025-05-11 05:53:10 +0100

    Changed the city to Sintra

--- src/index.js
+++ src/index.js
@@ -101,4 +101,4 @@ function displayForecast(response) {
 let form = document.querySelector("#city-search-form");
 form.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Cape Town");
+searchCity("Sintra");
